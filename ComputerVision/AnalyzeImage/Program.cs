256a4bf4f8f832e3a0c60e41a38b42111602f3a0
atteco <atte.collan@gmail.com>
2018-10-18 12:53:04 +0300

    testing5

---
+++ ComputerVision/AnalyzeImage/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageAnalyze
+{
+    class Program
+    {
+        // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+        private const string subscriptionKey = "e1c149a5b1df414e9be79ce2b87ef446";
+
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"D:\Pictures\kani1.jpg";
+
+        private const string remoteImageUrl =
+            "http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg";
+
+        // Specify the features to return
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags
+        };
+
+        static void Main(string[] args)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+
+            // You must use the same region as you used to get your subscription
+            // keys. For example, if you got your subscription keys from westus,
+            // replace "westcentralus" with "westus".
+            //
+            // Free trial subscription keys are generated in the westcentralus
+            // region. If you use a free trial subscription key, you shouldn't
+            // need to change the region.
+
+            // Specify the Azure region
+            computerVision.Endpoint = "https://northeurope.api.cognitive.microsoft.com/";
+
+            Console.WriteLine("Images being analyzed ...");
+            var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
+            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
+
+            Task.WhenAll(t1, t2).Wait(5000);
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        // Analyze a remote image
+        private static async Task AnalyzeRemoteAsync(
+            ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine(
+                    "\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return;
+            }
+
+            ImageAnalysis analysis =
+                await computerVision.AnalyzeImageAsync(imageUrl, features);
+            DisplayResults(analysis, imageUrl);
+        }
+
+        // Analyze a local image
+        private static async Task AnalyzeLocalAsync(
+            ComputerVisionClient computerVision, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine(
+                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                return;
+            }
+
+            using (Stream imageStream = File.OpenRead(imagePath))
+            {
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                    imageStream, features);
+                DisplayResults(analysis, imagePath);
+            }
+        }
+
+        // Display the most relevant caption for the image
+        private static void DisplayResults(ImageAnalysis analysis, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+        }
+    }
+}
